a5313932bbb62bf09bb8b0575d942b3a41e63fa1
kathirayyanesh <kathirayyanesh2006@gmail.com>
2026-03-19 21:34:27 +0530

    Change the input space and make the output in int

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ db = pd.read_csv("Data/Housing_last.csv")
 db.head()
 
 db = db.dropna(axis = 0)
-X = db.drop("price" , axis=1)
+X = db.drop(["price", "basement"] , axis=1)
 y = db["price"]
 
 #model = GradientBoostingRegressor( n_estimators=200, learning_rate=0.05 , max_depth=3 , random_state=42)
@@ -19,27 +19,25 @@ model.fit(X_train,y_train)
 st.title("House Price Prediction Model ")
 st.write("Enter the house detail and press the Predict buttonnnnn")
 
-area    = st.number_input("Area(sq.feet):")
-bedroom = st.number_input("No Bedrooms:")
-bathroom = st.number_input("No Batrhrooms:")
-guestroom = st.number_input("No GuestRooms:")
-basement = st.number_input("Basement:")
-parking = st.number_input("Parking:")
-furnishingstatus = st.number_input("Furnising(0,1,2):")
+area    = st.text_input("Area(sq.feet): " , placeholder="Enter the Area")
+bedroom = st.text_input("No Bedrooms:", placeholder="Enter the No of Bedrooms")
+bathroom = st.text_input("No Batrhrooms:",placeholder="Enter the No of Bathrooms")
+guestroom = st.text_input("No GuestRooms:",placeholder="Enter the No of Guestrooms")
+parking = st.text_input("Parking:",placeholder="Enter the No of Parking")
+furnishingstatus = st.text_input("Furnising(0,1,2):",placeholder="Enter the FurnishingStatus")
 
 if st.button("Predict Price "):
     
-    features = pd.DataFrame([[area,bedroom,bathroom,guestroom,basement,parking,furnishingstatus]],
+    features = pd.DataFrame([[float(area),int(bedroom),int(bathroom),int(guestroom),int(parking),int(furnishingstatus)]],
     columns=[
     "area",
     "bedrooms",
     "bathrooms",
     "guestroom",
-    "basement",
     "parking",
     "furnishingstatus"
     ])
 
-    prediction = model.predict(features)
+    prediction = int(model.predict(features)[0])
 
     st.success(f"Predicted Price : {(prediction)}")
